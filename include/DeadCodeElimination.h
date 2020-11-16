b35e58406d72b9b1d5e6bd28e426aebaa851387a
gandhi56 <gandhi21299@gmail.com>
2020-11-16 12:29:01 -0700

    removed legacy PM

--- include/DeadCodeElimination.h
+++ include/DeadCodeElimination.h
@@ -8,18 +8,9 @@
 #include "llvm/IR/PassManager.h"
 #include "llvm/Pass.h"
 
-/*
- * FIXME New pass manager version, not working
 struct DeadCodeElimination: public llvm::PassInfoMixin<DeadCodeElimination>{
   llvm::PreservedAnalyses
   run(llvm::Function& fn, llvm::FunctionAnalysisManager&);
-};
-*/
-
-struct DeadCodeElimination : public llvm::FunctionPass {
-  static char ID;
-  DeadCodeElimination() : FunctionPass(ID) {}
-  bool runOnFunction(llvm::Function &fn) override;
 };
 
 
--- lib/DeadCodeElimination.cpp
+++ lib/DeadCodeElimination.cpp
@@ -7,10 +7,10 @@
  * Usage:
  *
  * Legacy PM
- *  opt -load cmake-build-debug/lib/libDeadCodeElimination.so `\` --dce <bitcode-file>
+ *  opt -load cmake-build-debug/lib/libDeadCodeElimination.so `\` --dead-code <bitcode-file>
  *
  * New PM
- *    opt -load-pass-plugin cmake-build-debug/lib/libDeadCodeElimination.so `\`
+ *    opt -load-pass-plugin cmake-build-debug/lib/libDeadCodeElimination.so
  *        -passes=-"dce" <bitcode-file>
  */
 
@@ -23,54 +23,33 @@
 #define PASS_NAME "dce"
 
 
-//llvm::PreservedAnalyses
-//DeadCodeElimination::run(llvm::Function& fn, llvm::FunctionAnalysisManager&){
-//  //=== pass implementation goes here ===//
-//  llvm::errs() << fn.getName() << "\n";
-//  return llvm::PreservedAnalyses::all();
-//}
+llvm::PreservedAnalyses
+DeadCodeElimination::run(llvm::Function& fn, llvm::FunctionAnalysisManager&){
+  //=== pass implementation goes here ===//
+  llvm::errs() << fn.getName() << "\n";
+  return llvm::PreservedAnalyses::all();
+}
 
 // --------------------------------------------------------------------------------
 // Pass Manager registration
 // --------------------------------------------------------------------------------
 
-//llvm::PassPluginLibraryInfo getDCEPluginInfo() {
-//  return {LLVM_PLUGIN_API_VERSION, PASS_NAME, LLVM_VERSION_STRING,
-//          [](llvm::PassBuilder &PB) {
-//            PB.registerPipelineParsingCallback(
-//                [](llvm::StringRef Name, llvm::FunctionPassManager &FPM,
-//                   llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) {
-//                  if (Name == PASS_NAME) {
-//                    FPM.addPass(DeadCodeElimination());
-//                    return true;
-//                  }
-//                  return false;
-//                });
-//          }};
-//}
-//
-//extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
-//llvmGetPassPluginInfo() {
-//  return getDCEPluginInfo();
-//}
+llvm::PassPluginLibraryInfo getDCEPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, PASS_NAME, LLVM_VERSION_STRING,
+          [](llvm::PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](llvm::StringRef Name, llvm::FunctionPassManager &FPM,
+                   llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) {
+                  if (Name == "dead-code") {
+                    FPM.addPass(DeadCodeElimination());
+                    return true;
+                  }
+                  return false;
+                });
+          }};
+}
 
-
-//============================== Legacy pass manager version ============================================//
-
-//-----------------------------------------------------------------------------
-// Legacy PM Registration
-//-----------------------------------------------------------------------------
-char DeadCodeElimination::ID = 0;
-
-static llvm::RegisterPass<DeadCodeElimination> X(/*PassArg=*/"dead-code",
-    /*Name=*/PASS_NAME,
-    /*CFGOnly=*/true,
-    /*is_analysis=*/false);
-
-//-----------------------------------------------------------------------------
-// Dead code elimination
-//-----------------------------------------------------------------------------
-bool DeadCodeElimination::runOnFunction(llvm::Function &fn) {
-  llvm::errs() << fn.getName() << '\n';
-  return false;
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return getDCEPluginInfo();
 }
